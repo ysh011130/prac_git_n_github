2d084d760d4b3ab4cabe7fe114136ae6131bfb90
yeom <yeom@gmail.com>
2025-11-26 05:31:59 +0000

    fixed index.ts

--- index.ts
+++ index.ts
@@ -1,3 +1,15 @@
-function func(x : number) : number {
-    return x * 2;
+class Person {
+	name : string;
+	age : number;
+	constructor(name : string, age : number) {
+		this.name = name;
+		this.age = age;
+	}
+
+	myName() {
+		console.log("My name is " + this.name);
+	}
+	myAge() {
+		console.log("I'm " + this.age + "years old");
+	}
 }
